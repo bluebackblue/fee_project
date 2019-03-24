acfc65fede632c779134f3fd04ecdccdd71f03dc
back blue <blueback28@gmail.com>
2019-03-24 19:31:58 +0900

    整理

---
+++ unity_2018_3/build_webgl.bat
@@ -0,0 +1,13 @@
+SET HOME=%~dp0
+CD ..\..\
+CALL setting.bat
+CD %HOME%
+
+SET PRIJECT_PATH=%HOME%
+SET OUTPUT_PATH=%HOME%
+
+@REM ---------------------------------------
+@REM WebGL
+@REM ---------------------------------------
+%UNITY_2018_3% -quit -batchmode -logFile .\build.log -projectPath %PRIJECT_PATH% -executeMethod Fee.UnityInitialize.Build.Build_WebGL ---outputpath %OUTPUT_PATH%
+@REM ---------------------------------------
---
+++ unity_2019_2/build_android.bat
@@ -0,0 +1,13 @@
+SET HOME=%~dp0
+CD ..\..\
+CALL setting.bat
+CD %HOME%
+
+SET PRIJECT_PATH=%HOME%
+SET OUTPUT_PATH=%HOME%
+
+@REM ---------------------------------------
+@REM Android
+@REM ---------------------------------------
+%UNITY_2019_2% -quit -batchmode -logFile .\build.log -projectPath %PRIJECT_PATH% -executeMethod Fee.UnityInitialize.Build.Build_Android ---outputpath %OUTPUT_PATH%
+@REM ---------------------------------------
