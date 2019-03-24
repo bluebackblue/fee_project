562c680e4874aeea96d77a12f8fddd6b24a416bd
back blue <blueback28@gmail.com>
2019-03-24 19:28:23 +0900

    整理

--- unity_2018_3/build.bat
+++ unity_2018_3/build.bat
@@ -9,7 +9,7 @@ SET OUTPUT_PATH=%HOME%
 @REM ---------------------------------------
 @REM WebGL
 @REM ---------------------------------------
-@REM %UNITY_2018_3% -quit -batchmode -logFile .\build.log -projectPath %PRIJECT_PATH% -executeMethod Fee.UnityInitialize.Build.Build_WebGL ---outputpath %OUTPUT_PATH%
+%UNITY_2018_3% -quit -batchmode -logFile .\build.log -projectPath %PRIJECT_PATH% -executeMethod Fee.UnityInitialize.Build.Build_WebGL ---outputpath %OUTPUT_PATH%
 @REM ---------------------------------------
 
 @REM ---------------------------------------
@@ -21,8 +21,6 @@ SET OUTPUT_PATH=%HOME%
 @REM ---------------------------------------
 @REM StandAloneWindows
 @REM ---------------------------------------
-@REM %UNITY_2018_3% -quit -batchmode -logFile .\build.log -projectPath %PRIJECT_PATH% -executeMethod Fee.UnityInitialize.Build.Build_StandAloneWindows ---outputpath %OUTPUT_PATH%
+%UNITY_2018_3% -quit -batchmode -logFile .\build.log -projectPath %PRIJECT_PATH% -executeMethod Fee.UnityInitialize.Build.Build_StandAloneWindows ---outputpath %OUTPUT_PATH%
 @REM ---------------------------------------
-
-
 
