09b1a6c6b74bb56d9424c0925db1e7ba7de697a1
backblue <blueback28@gmail.com>
2019-05-20 22:51:54 +0900

    InputSystem

--- unity_2019_2_0a11/Assets/Fee/Script/Dijkstra/Link.cs
+++ unity_2019_2_0a11/Assets/Fee/Script/Dijkstra/Link.cs
@@ -40,28 +40,18 @@ namespace Fee.Dijkstra
 		*/
 		private Node<NODEKEY,NODEDATA,LINKDATA> to_node;
 
-		/** 接続先ノードへの到達コスト。
-		*/
-		/*
-		private long to_cost;
-		*/
-
 		/** constructor
 		*/
 		public Link(LINKDATA a_linkdata,Node<NODEKEY,NODEDATA,LINKDATA> a_to_node)
 		{
 			this.linkdata = a_linkdata;
 			this.to_node = a_to_node;
-
-			//this.to_cost = a_to_cost;
-			//this.linkdata.SetToCost(a_to_cost);
 		}
 
 		/** 接続先ノードへの到達コスト。設定。
 		*/
 		public void SetToCost(Fee.Dijkstra.Instance_Base a_instance,long a_to_cost)
 		{
-			//this.to_cost = a_to_cost;
 			this.linkdata.SetToCost(a_instance,a_to_cost);
 		}
 
@@ -69,7 +59,6 @@ namespace Fee.Dijkstra
 		*/
 		public long GetToCost(Fee.Dijkstra.Instance_Base a_instance)
 		{
-			//return this.to_cost;
 			return this.linkdata.GetToCost(a_instance);
 		}
 
