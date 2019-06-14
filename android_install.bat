542370b666f284a9803c9842d2230fb8a17834c0
backblue <blueback28@gmail.com>
2019-06-15 02:52:20 +0900

    整理

---
+++ android_install.bat
@@ -0,0 +1,15 @@
+SET HOME=%~dp0
+CD ..\..\
+CALL setting.bat
+CD %HOME%
+
+%ANDROID_ADB% kill-server
+
+%ANDROID_ADB% devices
+
+%ANDROID_ADB% install .\result\unity_2018_3.apk
+%ANDROID_ADB% install .\result\unity_2018_4.apk
+%ANDROID_ADB% install .\result\unity_2019_1.apk
+%ANDROID_ADB% install .\result\unity_2019_2.apk
+
+@PAUSE
