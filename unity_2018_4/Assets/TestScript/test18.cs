172c8e84c32b2eef343932ece685da25e994125a
backblue <blueback28@gmail.com>
2019-08-26 06:56:59 +0900

    視錐台カリング

--- unity_2018_4/Assets/TestScript/test18.cs
+++ unity_2018_4/Assets/TestScript/test18.cs
@@ -28,7 +28,7 @@ namespace TestScript
 				"test18",
 
 				@"
-				---
+				視錐台カリング
 				"
 			);
 		}
@@ -36,6 +36,18 @@ namespace TestScript
 		/** 削除管理。
 		*/
 		private Fee.Deleter.Deleter deleter;
+
+		/** frustum_culling
+		*/
+		private Fee.Geometry.FrustumCulling frustum_culling;
+
+		/** main_camera
+		*/
+		private UnityEngine.Camera main_camera;
+
+		/** cube
+		*/
+		private UnityEngine.GameObject cube;
 
 		/** Start
 		*/
@@ -77,6 +89,19 @@ namespace TestScript
 			//削除管理。
 			this.deleter = new Fee.Deleter.Deleter();
 
+			//frustum_culling
+			this.frustum_culling = new Fee.Geometry.FrustumCulling();
+
+			//main_camera
+			this.main_camera = GameObject.Find("Main Camera").GetComponent<UnityEngine.Camera>();
+			this.main_camera.depth = Fee.Render2D.Render2D.GetInstance().GetCameraAfterDepth(Fee.Render2D.Config.MAX_LAYER);
+
+			//キューブ。
+			{
+				GameObject t_prefab = Resources.Load<GameObject>(Data.Resources.PREFAB_CUBE);
+				this.cube = GameObject.Instantiate(t_prefab,Vector3.zero,Quaternion.identity);
+			}
+
 			//戻るボタン作成。
 			this.CreateReturnButton(this.deleter,(Fee.Render2D.Render2D.MAX_LAYER - 1) * Fee.Render2D.Render2D.DRAWPRIORITY_STEP,this.name + ":Return");
 		}
@@ -93,6 +118,23 @@ namespace TestScript
 
 			//ＵＩ。
 			Fee.Ui.Ui.GetInstance().Main();
+
+			//プレーンリスト更新。
+			this.frustum_culling.UpdatePlaneList(this.main_camera);
+
+			//キューブの位置。
+			{
+				float t_x = (Fee.Input.Mouse.GetInstance().cursor.pos.x - Fee.Render2D.Config.VIRTUAL_W  / 2) / 50.0f;
+				float t_y = (Fee.Input.Mouse.GetInstance().cursor.pos.y - Fee.Render2D.Config.VIRTUAL_H  / 2) / 50.0f;
+				this.cube.transform.position = new Vector3(t_x,-t_y,0);
+			}
+			
+			//カリングテスト。
+			if(this.frustum_culling.CullingTest(this.cube.transform.position) == true){
+				this.cube.GetComponent<MeshRenderer>().material.color = new Color(1.0f,0.0f,0.0f,1.0f);
+			}else{
+				this.cube.GetComponent<MeshRenderer>().material.color = new Color(0.0f,0.0f,1.0f,1.0f);
+			}
 		}
 
 		/** 削除前。
