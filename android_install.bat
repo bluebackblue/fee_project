b17a32231369d192703b33269232f362b8c608c3
backblue <blueback28@gmail.com>
2019-12-25 16:04:43 +0900

    整理

--- android_install.bat
+++ android_install.bat
@@ -7,9 +7,7 @@ CD %HOME%
 
 %ANDROID_ADB% devices
 
-%ANDROID_ADB% install -r .\result\unity_2018_3.apk
-%ANDROID_ADB% install -r .\result\unity_2018_4.apk
-%ANDROID_ADB% install -r .\result\unity_2019_1.apk
 %ANDROID_ADB% install -r .\result\unity_2019_2.apk
+%ANDROID_ADB% install -r .\result\unity_2020_1.apk
 
 @PAUSE
