1dd30552418afcf8a4f8b89ee1263ac0d4dd7293
backblue <blueback28@gmail.com>
2020-01-11 16:48:44 +0900

    movie

--- unity_2020_1/Assets/TestScript/test22.cs
+++ unity_2020_1/Assets/TestScript/test22.cs
@@ -41,13 +41,17 @@ namespace TestScript
 		*/
 		private Fee.Render2D.Sprite2D bg_sprite;
 
-		/** video_sprite
+		/** movie
 		*/
-		private Fee.Render2D.Sprite2D video_sprite;
-
-		/** video_player
-		*/
-		private UnityEngine.Video.VideoPlayer video_player;
+		private Fee.Movie.MovieItem movie_item_1;
+		private Fee.Movie.MovieItem movie_item_2;
+		private Fee.Movie.MovieItem movie_item_3;
+		private Fee.Render2D.Sprite2D movie_sprite_1;
+		private Fee.Render2D.Sprite2D movie_sprite_2;
+		private Fee.Render2D.Sprite2D movie_sprite_3;
+		private Fee.Render2D.Text2D movie_text_1;
+		private Fee.Render2D.Text2D movie_text_2;
+		private Fee.Render2D.Text2D movie_text_3;
 
 		/** Start
 		*/
@@ -84,6 +88,9 @@ namespace TestScript
 			Fee.Ui.Config.LOG_ENABLE = true;
 			Fee.Ui.Ui.CreateInstance();
 
+			//ムービー。インスタンス作成。
+			Fee.Movie.Movie.CreateInstance();
+
 			//フォント。
 			Font t_font = Resources.Load<Font>(Data.Resources.FONT);
 			if(t_font != null){
@@ -102,35 +109,49 @@ namespace TestScript
 				this.bg_sprite.SetRect(in Fee.Render2D.Config.VIRTUAL_RECT_MAX);
 				this.bg_sprite.SetTextureRect(in Fee.Render2D.Config.TEXTURE_RECT_MAX);
 				this.bg_sprite.SetTexture(UnityEngine.Texture2D.whiteTexture);
-				this.bg_sprite.SetColor(1.0f,0.0f,0.0f,1.0f);
+				this.bg_sprite.SetColor(0.2f,0.0f,0.0f,1.0f);
 			}
 			
-			{
-				//レンダーテクスチャ。
-				UnityEngine.RenderTexture t_render_texture;
-				t_render_texture = new RenderTexture(Fee.Render2D.Config.VIRTUAL_W,Fee.Render2D.Config.VIRTUAL_H,0,UnityEngine.RenderTextureFormat.RGB565,UnityEngine.RenderTextureReadWrite.Default);
-				t_render_texture.Create();
+			//movie_texture
+			this.movie_item_1 = Fee.Movie.MovieItem.Create(this.deleter,Fee.Render2D.Config.VIRTUAL_W,Fee.Render2D.Config.VIRTUAL_H,Fee.File.Path.CreateStreamingAssetsPath(new Fee.File.Path("Test22/video.mp4")));
+			this.movie_item_1.SetLoop(false);
 
-				//ビデオプレイヤー。
-				UnityEngine.GameObject t_gameobject = new GameObject("videoplayer");
-				t_gameobject.AddComponent<UnityEngine.Video.VideoPlayer>();
-				this.video_player = t_gameobject.GetComponent<UnityEngine.Video.VideoPlayer>();
-				this.video_player.renderMode = UnityEngine.Video.VideoRenderMode.RenderTexture;
-				this.video_player.isLooping = true;
-				this.video_player.playOnAwake = false;
-				this.video_player.waitForFirstFrame = true;
-				this.video_player.skipOnDrop = true;
-				this.video_player.targetTexture = t_render_texture;
-				this.video_player.source = UnityEngine.Video.VideoSource.Url;
-				this.video_player.url = Fee.File.Path.CreateStreamingAssetsPath(new Fee.File.Path("Test22/video.mp4")).GetPath();
+			this.movie_item_2 = Fee.Movie.MovieItem.Create(this.deleter,Fee.Render2D.Config.VIRTUAL_W,Fee.Render2D.Config.VIRTUAL_H,Fee.File.Path.CreateStreamingAssetsPath(new Fee.File.Path("Test22/video.mp4")));
+			this.movie_item_2.SetLoop(false);
 
-				//表示先。
-				this.video_sprite = Fee.Render2D.Sprite2D.Create(this.deleter,1);
-				this.video_sprite.SetRect(in Fee.Render2D.Config.VIRTUAL_RECT_MAX);
-				this.video_sprite.SetTextureRect(in Fee.Render2D.Config.TEXTURE_RECT_MAX);
-				this.video_sprite.SetTexture(t_render_texture);
-				this.video_sprite.SetColor(1.0f,1.0f,1.0f,1.0f);
-			}
+			this.movie_item_3 = Fee.Movie.MovieItem.Create(this.deleter,Fee.Render2D.Config.VIRTUAL_W,Fee.Render2D.Config.VIRTUAL_H,Fee.File.Path.CreateStreamingAssetsPath(new Fee.File.Path("Test22/video.mp4")));
+			this.movie_item_3.SetLoop(false);
+
+			//表示先。
+			this.movie_sprite_1 = Fee.Render2D.Sprite2D.Create(this.deleter,1);
+			this.movie_sprite_1.SetRect(100,100,300,300);
+			this.movie_sprite_1.SetTextureRect(in Fee.Render2D.Config.TEXTURE_RECT_MAX);
+			this.movie_sprite_1.SetTexture(this.movie_item_1.GetRenderTexture());
+			this.movie_sprite_1.SetColor(1.0f,1.0f,1.0f,1.0f);
+
+			//表示先。
+			this.movie_sprite_2 = Fee.Render2D.Sprite2D.Create(this.deleter,2);
+			this.movie_sprite_2.SetRect(300,200,300,300);
+			this.movie_sprite_2.SetTextureRect(in Fee.Render2D.Config.TEXTURE_RECT_MAX);
+			this.movie_sprite_2.SetTexture(this.movie_item_2.GetRenderTexture());
+			this.movie_sprite_2.SetColor(1.0f,1.0f,1.0f,1.0f);
+
+			//表示先。
+			this.movie_sprite_3 = Fee.Render2D.Sprite2D.Create(this.deleter,3);
+			this.movie_sprite_3.SetRect(500,300,300,300);
+			this.movie_sprite_3.SetTextureRect(in Fee.Render2D.Config.TEXTURE_RECT_MAX);
+			this.movie_sprite_3.SetTexture(this.movie_item_3.GetRenderTexture());
+			this.movie_sprite_3.SetColor(1.0f,1.0f,1.0f,1.0f);
+
+
+			this.movie_text_1 = Fee.Render2D.Text2D.Create(this.deleter,1);
+			this.movie_text_1.SetRect(this.movie_sprite_1.GetX(),this.movie_sprite_1.GetY() - 30,0,0);
+
+			this.movie_text_2 = Fee.Render2D.Text2D.Create(this.deleter,1);
+			this.movie_text_2.SetRect(this.movie_sprite_2.GetX(),this.movie_sprite_2.GetY() - 30,0,0);
+
+			this.movie_text_3 = Fee.Render2D.Text2D.Create(this.deleter,1);
+			this.movie_text_3.SetRect(this.movie_sprite_3.GetX(),this.movie_sprite_3.GetY() - 30,0,0);
 		}
 
 		/** FixedUpdate
@@ -150,14 +171,18 @@ namespace TestScript
 			Fee.Ui.Ui.GetInstance().Main();
 
 			if(Fee.Input.Mouse.GetInstance().left.down == true){
-				this.video_player.Play();
-
-				UnityEngine.Debug.Log("Play");
+				this.movie_item_1.Play();
+				this.movie_item_2.Play();
+				this.movie_item_3.Play();
 			}else if(Fee.Input.Mouse.GetInstance().right.down == true){
-				this.video_player.Pause();
-
-				UnityEngine.Debug.Log("Pause");
+				this.movie_item_1.Pause();
+				this.movie_item_2.Pause();
+				this.movie_item_3.Pause();
 			}
+
+			this.movie_text_1.SetText(this.movie_item_1.GetFrame().ToString() + " " + this.movie_item_1.GetFrameMax().ToString() + " " + this.movie_item_1.GetTime().ToString("0.000") + " " + this.movie_item_1.GetFrameRate().ToString("0.000")+ " " + this.movie_item_1.IsPrepared().ToString() + " " + this.movie_item_1.IsPlay().ToString() + " " + this.movie_item_1.IsPause().ToString());
+			this.movie_text_2.SetText(this.movie_item_2.GetFrame().ToString() + " " + this.movie_item_2.GetFrameMax().ToString() + " " + this.movie_item_2.GetTime().ToString("0.000") + " " + this.movie_item_2.GetFrameRate().ToString("0.000")+ " " + this.movie_item_2.IsPrepared().ToString() + " " + this.movie_item_2.IsPlay().ToString() + " " + this.movie_item_2.IsPause().ToString());
+			this.movie_text_3.SetText(this.movie_item_3.GetFrame().ToString() + " " + this.movie_item_3.GetFrameMax().ToString() + " " + this.movie_item_3.GetTime().ToString("0.000") + " " + this.movie_item_3.GetFrameRate().ToString("0.000")+ " " + this.movie_item_3.IsPrepared().ToString() + " " + this.movie_item_3.IsPlay().ToString() + " " + this.movie_item_3.IsPause().ToString());
 
 			//２Ｄ描画。
 			Fee.Render2D.Render2D.GetInstance().Main_After();
