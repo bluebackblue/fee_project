c3e1cd67e3526434858a5e2b267de68447aabe5f
backblue <blueback28@gmail.com>
2020-01-14 06:51:52 +0900

    audio bank 整理 rapid

--- unity_2020_1/Assets/ThirdParty/Open/Fee/Script/Instantiate/MaterialList_MonoBehaviour.cs
+++ unity_2020_1/Assets/ThirdParty/Open/Fee/Script/Instantiate/MaterialList_MonoBehaviour.cs
@@ -22,38 +22,6 @@ namespace Fee.Instantiate
 		*/
 		[UnityEngine.SerializeField]
 		public UnityEngine.Material[] material_list;
-
-		/** list
-		*/
-		public System.Collections.Generic.Dictionary<string,UnityEngine.Material> list;
-
-		/** 初期化。
-		*/
-		public void Initialize()
-		{
-			try{
-				this.list = new System.Collections.Generic.Dictionary<string,UnityEngine.Material>();
-				for(int ii=0;ii<this.tag_list.Length;ii++){
-					this.list.Add(this.tag_list[ii],this.material_list[ii]);
-				}
-			}catch(System.Exception t_exception){
-				Tool.DebugReThrow(t_exception);
-			}
-		}
-
-		/** 取得。
-		*/
-		public UnityEngine.Material GetItem(string a_tag)
-		{
-			UnityEngine.Material t_value;
-
-			if(this.list.TryGetValue(a_tag,out t_value) == false){
-				Tool.Assert(false);
-				t_value = null;
-			}
-
-			return t_value;
-		}
 	}
 }
 
