7ecc8293dce2ae218b3bbdea38dafa6d6d3a3243
blueback <blueback28@gmail.com>
2020-06-04 22:57:56 +0900

    playerloopsystem : input / file / eventplate / ui

---
+++ unity_2019_2/Assets/ThirdParty/Open/Fee/Script/File/PlayerLoopSystemType.cs
@@ -0,0 +1,26 @@
+
+
+/**
+ * Copyright (c) blueback
+ * Released under the MIT License
+ * https://github.com/bluebackblue/fee/blob/master/LICENSE.txt
+ * @brief ファイル。
+*/
+
+
+/** Fee.File
+*/
+namespace Fee.File
+{
+	/** PlayerLoopSystemType
+	*/
+	public struct PlayerLoopSystemType
+	{
+		/** Fee_File_Main
+		*/
+		public struct Fee_File_Main
+		{
+		}
+	}
+}
+
