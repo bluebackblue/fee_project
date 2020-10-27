6b0e5ceb5594e3caea79a2878be66a410e2d8aa7
blueback <blueback28@gmail.com>
2020-10-27 19:31:31 +0900

    ポーズ

--- unity_2020_1/Assets/TestScript/test24.cs
+++ unity_2020_1/Assets/TestScript/test24.cs
@@ -25,7 +25,7 @@ namespace TestScript
 				"test24",
 
 				@"
-				---
+				ポーズ
 				"
 			);
 		}
@@ -37,6 +37,22 @@ namespace TestScript
 		/** prefablist
 		*/
 		private Common.PrefabList prefablist;
+
+		/** text
+		*/
+		private Fee.Render2D.Text2D text;
+
+		/** time
+		*/
+		private float time_update;
+		private float time_fixedupdate;
+		private float time_rowupdate;
+
+		/** count
+		*/
+		private int count_update;
+		private int count_fixedupdate;
+		private int count_rowupdate;
 
 		/** Start
 		*/
@@ -56,7 +72,8 @@ namespace TestScript
 			Fee.PerformanceCounter.Config.LOG_ENABLE = true;
 			Fee.PerformanceCounter.PerformanceCounter.CreateInstance();
 
-			//関数呼び出し。
+			//関数呼び出し。インスタンス作成。
+			Fee.Function.Config.ROWUPDATE_DELTA = 1 / 20.0f;
 			Fee.Function.Function.CreateInstance();
 			Fee.Function.Function.GetInstance().SetMonoBehaviour(this);
 			Fee.Function.Function.GetInstance().SetRowUpdate(this.RowUpdate);
@@ -71,13 +88,16 @@ namespace TestScript
 			Fee.Input.Config.LOG_ENABLE = true;
 			Fee.Input.Input.CreateInstance(true,false,true,false);
 
-			//イベントプレート。
+			//イベントプレート。インスタンス作成。
 			Fee.EventPlate.Config.LOG_ENABLE = true;
 			Fee.EventPlate.EventPlate.CreateInstance();
 
 			//ＵＩ。インスタンス作成。
 			Fee.Ui.Config.LOG_ENABLE = true;
 			Fee.Ui.Ui.CreateInstance();
+
+			//ポーズ。インスタンス作成。
+			Fee.Pause.Pause.CreateInstance();
 
 			//プレハブリスト。
 			this.prefablist = new Common.PrefabList();
@@ -90,24 +110,74 @@ namespace TestScript
 
 			//戻るボタン作成。
 			this.CreateReturnButton(this.prefablist,this.deleter,(Fee.Render2D.Config.MAX_LAYER - 1) * Fee.Render2D.Config.DRAWPRIORITY_STEP,this.name + ":Return");
+
+			//text
+			this.text = Fee.Render2D.Text2D.Create(this.deleter,0);
+			this.text.SetRect(100,100,0,0);
+
+			//time
+			this.time_update = 0.0f;
+			this.time_fixedupdate = 0.0f;
+			this.time_rowupdate = 0.0f;
+
+			//count
+			this.count_update = 0;
+			this.count_fixedupdate = 0;
+			this.count_rowupdate = 0;
+
+			//fixedDeltaTime
+			UnityEngine.Time.fixedDeltaTime = 1.0f / 60;
 		}
 
 		/** RowUpdate
 		*/
 		private void RowUpdate()
 		{
+			this.time_rowupdate += Fee.Function.Config.ROWUPDATE_DELTA * Fee.Pause.Pause.GetInstance().GetTimeScale();
+			this.count_rowupdate++;
 		}
 
 		/** FixedUpdate
 		*/
 		private void FixedUpdate()
 		{
+			this.time_fixedupdate += UnityEngine.Time.fixedDeltaTime;
+			this.count_fixedupdate++;
 		}
 
 		/** Update
 		*/
 		private void Update()
 		{
+			this.time_update += UnityEngine.Time.deltaTime;
+			this.count_update++;
+
+			this.text.SetText(
+				"deltatime = " + UnityEngine.Time.deltaTime.ToString() + "\n" +
+
+				"time_update = " + this.time_update.ToString()  + "\n" +
+				"time_fixedupdate = " + this.time_fixedupdate.ToString()  + "\n" +
+				"time_rowupdate = " + this.time_rowupdate.ToString()  + "\n" +
+
+				"count_update = " + this.count_update.ToString()  + "\n" +
+				"count_fixedupdate = " + this.count_fixedupdate.ToString()  + "\n" +
+				"count_rowupdate = " + this.count_rowupdate.ToString()  + "\n"
+			);
+			
+			if(Fee.Input.Input.GetInstance().mouse.right.down == true){
+				if(Fee.Pause.Pause.GetInstance().GetTimeScale() > 0.0f){
+					//停止。
+					Fee.Pause.Pause.GetInstance().SetNextFrameTimeScale(0.0f);
+				}else{
+					//再生。
+					Fee.Pause.Pause.GetInstance().SetNextFrameTimeScale(1.0f);
+				}
+			}
+
+			if(Fee.Input.Input.GetInstance().mouse.left.down == true){
+				//ステップ。
+				Fee.Pause.Pause.GetInstance().StepOneFrame(1.0f);
+			}
 		}
 
 		/** LateUpdate
